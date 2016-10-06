f02f143e0b482dfe76d0cf5aa97f9ca6a12341c5
Marlon Huber-Smith <Marlon.Huber-Smith@akqa.com>
2016-10-06 10:17:17 +0200

    Remapped data coming from API

--- controllers/times.js
+++ controllers/times.js
@@ -1,29 +1,44 @@
 import fetch from 'node-fetch'
 
 const API_ENDPOINT = `http://www.vasttrafik.se/CustomerServices/EPiServerWs/Service.svc/GetNextTrips`
+const REQUEST = {
+	method: `POST`,
+	headers: {
+		[`Content-Type`]: `application/json`,
+	},
+	body: '{"request":{"RDC_Language":"sv-SE","StopAreaExternalId":"9021014004493000"}}',
+}
+
+const DESTINATIONS_TO_STENPIREN = [
+	`Lilla Bommen via Stenpiren`,
+	`Stenpiren, Fri resa`,
+]
+
+const filterBoatsToStenpiren = boat => {
+	return DESTINATIONS_TO_STENPIREN.indexOf(boat.Destination) > -1
+}
+
+const mapUsefulInfo = boat => {
+	return {
+		destination: boat.Destination,
+		number: boat.Line.Designation,
+		next: boat.NextDepartureTime,
+		after: boat.NextNextDepartureTime,
+	}
+}
+
+const parse = json => {
+	return json.Items
+		.filter(filterBoatsToStenpiren)
+		.map(mapUsefulInfo)
+}
 
 export default {
 	get: async ctx => {
-		fetch(API_ENDPOINT, {
-			method: `POST`,
-			headers: {
-				[`Content-Type`]: `application/json`,
-			},
-			body: {"request":{"RDC_Language":"sv-SE","StopAreaExternalId":"9021014004493000"}},
-		}) 
-			.then(res => { 
-				console.log('got res: ', res)
-				return res.json()
-			})
-			.then(json => {
-				console.log(`fetched: `, json)
-			})
-			.catch(err => { 
-				console.log('fail', err) 
-			})
-		
-		ctx.body = {
-			lindholmspiren : [1, 3, 5],
-		}
+		ctx.body = await fetch(API_ENDPOINT, REQUEST)
+			.then(res => res.json())
+			.then(json => json.d)
+			.then(json => parse(json))
+			.catch(err => err)
 	}
 }
