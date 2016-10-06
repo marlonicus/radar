2e242a4bf2efbe2ba3332457b5348b8a3a8ceb3e
Marlon Huber-Smith <Marlon.Huber-Smith@akqa.com>
2016-10-06 02:48:12 +0200

    Started fetch implementation for API call, looks like their service has changed recently though so it's not working. Updated readme (FWIW).

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+# Radar
 A proxy service for retrieving the latest boat times to and from Lindholmspiren (Gothenburg). 
 
+## Getting Started
+These instructions will get the project up and running on your local machine for development purposes.
+
+### Prerequisites
+- Node.js
+- NPM
+
+### Installing
+```
+npm install
+```
+
+### Running
+```
+npm start
+```
--- controllers/times.js
+++ controllers/times.js
@@ -1,5 +1,27 @@
+import fetch from 'node-fetch'
+
+const API_ENDPOINT = `http://www.vasttrafik.se/CustomerServices/EPiServerWs/Service.svc/GetNextTrips`
+
 export default {
-	get: async (ctx) => {
+	get: async ctx => {
+		fetch(API_ENDPOINT, {
+			method: `POST`,
+			headers: {
+				[`Content-Type`]: `application/json`,
+			},
+			body: {"request":{"RDC_Language":"sv-SE","StopAreaExternalId":"9021014004493000"}},
+		}) 
+			.then(res => { 
+				console.log('got res: ', res)
+				return res.json()
+			})
+			.then(json => {
+				console.log(`fetched: `, json)
+			})
+			.catch(err => { 
+				console.log('fail', err) 
+			})
+		
 		ctx.body = {
 			lindholmspiren : [1, 3, 5],
 		}
